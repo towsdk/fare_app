61e7a75e1e82974dfd896c0f52bd9b19fb02ebc5
towhid-siddiqui <towsdk@gmail.com>
2023-03-13 00:04:10 +0600

    fare app firest creation

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,58 @@
+<div class="container">
+    <div class="pt-20">
+      <h2 class="section-heading">Information & Content </h2>
+        <div class="flex -mx-4">
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width:130px" class="bg-gray-500"></div>
+                    <div class="p-4">
+                            <h3>Work like a worker And Gain it</h3>
+                            <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width:130px" class="bg-gray-500"></div>
+                    <div class="p-4">
+                            <h3>Work like a worker And Gain it</h3>
+                            <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width:130px" class="bg-gray-500"></div>
+                    <div class="p-4">
+                            <h3>Work like a worker And Gain it</h3>
+                            <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    
+    </div>
+    <div class="pt-20 text-lg border-b-2 border-gray-500 pb-16 mb-20">
+        <h2 class="section-heading">More Information</h2>
+          <div class="flex -mx-4">
+              <div class="flex-1 px-4">
+                  <a href="" class="underline">Lorem, ipsum dolor.</a>
+              </div>
+              <div class="flex-1 px-4">
+                  <a href="" class="underline">Lorem, ipsum dolor.</a>
+              </div>
+              <div class="flex-1 px-4">
+                  <a href="" class="underline">Lorem, ipsum dolor.</a>
+              </div>
+              <div class="flex-1 px-4">
+                  <a href="" class="underline">Lorem, ipsum dolor.</a>
+              </div>
+          </div>
+      
+      </div>
+
+      <p>test</p>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-guest-layout>
+    <div class="relative z-10 pt-52 pb-64 bg-cover bg-center" style="background-image: url(/img/hero-bg1.png)">
+    <div class="absolute h-full w-full bg-black opacity-70 top-0 left-0 z-10">
+
+    </div>
+    <div class="container relative z-20 text-white text-center text-2xl">
+        <h2 class="font-bold text-5xl mb-8">Lorem ipsum dolor sit amet consectetur e minus,<br/>vident quod qui. Rem, dignissimos?</h2>
+        <p >Lorem ipsumoris qui est! Dolorum!</p>
+    </div>
+</div>
+</x-guest-layout>
